759d2424b726e73240531bb291c8c7507cd8aedc
Daniel Catori <caaatori@gmail.com>
2024-06-30 15:58:44 -0300

    JPA repository, dependency injection, database seeding

---
+++ src/main/java/com/catori/course/repositories/UserRepository.java
@@ -0,0 +1,9 @@
+package com.catori.course.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.catori.course.entities.User;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+}
